c6037dcb5a151a283cfcf9583facc2cc2807e436
Rohit Raj <rajrohit5783@gmail.com>
2026-08-26 07:28:05 +0530

    Pattern 2

---
+++ Pattern_02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void print(int n){
+   for (int i = 0; i < n ; i++){
+    for(int j = 0 ; j <= i ; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+
+   }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i = 0; i<t;i++){
+        int n;
+        cin>>n;
+        print(n);
+    }
+
+    return 0;
+}
