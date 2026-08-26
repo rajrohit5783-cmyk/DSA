b16d8df7337cf4c34ba550061cd8cbe1efda2f09
Rohit Raj <rajrohit5783@gmail.com>
2026-08-26 07:33:46 +0530

    Patterns

---
+++ Patterns/Pattern_05.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void print(int n){
+    for(int i = 1; i<= n; i++){
+        for(int j = 0; j<n-i+1; j++){
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i = 0; i<t;i++){
+        int n;
+        cin>>n;
+        print(n);
+    }
+
+    return 0;
+}
---
+++ Patterns/Pattern_08.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void print(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        print(n);
+    }
+
+    return 0;
+}
