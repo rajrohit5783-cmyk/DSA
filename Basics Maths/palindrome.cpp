87d2c216655aed741a60bc76a3474ff0e9055753
Rohit Raj <rajrohit5783@gmail.com>
2026-09-14 19:19:21 +0530

    Palindrome

---
+++ Basics Maths/palindrome.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    int revNum = 0;
+     int dup = n ;
+     while(n>10){
+        int ld = n%10;
+        revNum = (revNum *10) + ld;
+        n = n/10;
+
+     }
+    if(dup == revNum) cout<<"true";
+    else cout<<"False";
+}
